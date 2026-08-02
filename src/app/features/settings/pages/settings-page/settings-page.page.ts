014cfff44435a85e83cc48e99fb476ebc12decc5
RomanCatalin <cata.david21@yahoo.com>
2026-08-02 16:50:23 +0300

    Hotbar tweaks, settings page UI

--- src/app/features/settings/pages/settings-page/settings-page.page.ts
+++ src/app/features/settings/pages/settings-page/settings-page.page.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonButton, IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-settings-page',
   templateUrl: './settings-page.page.html',
   styleUrls: ['./settings-page.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButton]
 })
 export class SettingsPagePage implements OnInit {
 
